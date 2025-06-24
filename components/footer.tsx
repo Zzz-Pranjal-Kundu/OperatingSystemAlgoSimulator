fd42986b18d45218760d681a1a7c7b1bb6e954a0
Pranjal Kundu <pranjalkundu1149@gmail.com>
2025-06-24 14:07:29 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="w-full py-4 text-center text-lg text-white">
       <h1 className="underline">
-        {/* <Link href="https://github.com/kautilyadevaraj/SchedulingAlgorithmSimulator" target="_blank">
+        {/* <Link href="https://github.com/Zzz-Pranjal-Kundu/OperatingSystemAlgoSimulator" target="_blank">
           View Github Repository
         </Link> */}
       </h1>
